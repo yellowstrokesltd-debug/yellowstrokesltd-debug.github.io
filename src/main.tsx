f8a341aad6f39144c8eeaa8f6dfbbecadd25cea5
svitalis123 <vitalis.wilson@yahoo.com>
2026-01-28 12:07:11 +0300

    added posthog

--- src/main.tsx
+++ src/main.tsx
@@ -1,12 +1,25 @@
-
+import React from 'react';
 import { createRoot } from "react-dom/client";
 import App from "./app/App.tsx";
 import "./styles/index.css";
 
 import { HelmetProvider } from 'react-helmet-async';
+import { PostHogProvider } from 'posthog-js/react';
 
 createRoot(document.getElementById("root")!).render(
-  <HelmetProvider>
-    <App />
-  </HelmetProvider>
+  <React.StrictMode>
+    <HelmetProvider>
+      <PostHogProvider
+        apiKey={import.meta.env.VITE_PUBLIC_POSTHOG_KEY}
+        options={{
+          api_host: import.meta.env.VITE_PUBLIC_POSTHOG_HOST,
+          defaults: '2025-05-24',
+          capture_exceptions: true, // This enables capturing exceptions using Error Tracking, set to false if you don't want this
+          debug: import.meta.env.MODE === "development",
+        }}
+      >
+        <App />
+      </PostHogProvider>
+    </HelmetProvider>
+  </React.StrictMode>
 );
