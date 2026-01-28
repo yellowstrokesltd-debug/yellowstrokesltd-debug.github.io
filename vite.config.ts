78c36f0805076b381f2525141ec8dbdb18be6f50
svitalis123 <vitalis.wilson@yahoo.com>
2026-01-28 07:38:28 +0300

    Update gitignore and project config

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,7 @@ import { defineConfig } from 'vite'
 import path from 'path'
 import tailwindcss from '@tailwindcss/vite'
 import react from '@vitejs/plugin-react'
+import Sitemap from 'vite-plugin-sitemap'
 
 export default defineConfig({
   base: '/',
@@ -10,6 +11,16 @@ export default defineConfig({
     // Tailwind is not being actively used – do not remove them
     react(),
     tailwindcss(),
+    Sitemap({
+      hostname: 'https://yellowstrokesltd-debug.github.io',
+      dynamicRoutes: [
+        '/about',
+        '/better-human-beings',
+        '/contact',
+        '/terms',
+        '/privacy'
+      ]
+    }),
   ],
   resolve: {
     alias: {
